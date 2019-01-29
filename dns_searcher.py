4651d3ffac34bd9fefff505f2aecb3a45cdf567c
oleggr <oleg.gr@outlook.com>
2019-01-29 19:43:01 +0300

    TODO is ready

--- dns_searcher.py
+++ dns_searcher.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-'''
+"""
 DNS domain searcher
 
 Приложение по поиску адресов фишинговых сайтов.
@@ -12,17 +12,19 @@ DNS domain searcher
 >>>python3 dns_searcher.py word1 word2 ... wordN
 
 Github: github.com/oleggr/another_one_resolver
-'''
+"""
 
 # TODO хотя бы ссылку на github в шапке добавил
 # TODO почему называется main.py ?
 # TODO пример использования в шапке а не в DomainNameSearcher (но и там можно оставить)!
 
 import sys
-# TODO нигде не исользуется. Рудимент. Зачем это?
+# TODO нигде не исользуется. Рудимент. Зачем это? (про import time)
 import string
+import datetime
 import threading
 
+from prettytable import PrettyTable
 import dns.resolver
 
 
@@ -32,8 +34,8 @@ __email__ = 'oleg.gr@outlook.com'
 
 # TODO если ни от кого не наследуется убери (). В Python3 так не пишут
 class DomainNameSearcher:
-    # TODO в docstring-ах нужно везде использовать """ вместо """ ! (ИСПРАВЛЕНО)
-    '''
+    # TODO в docstring-ах нужно везде использовать """ вместо ''' ! (ИСПРАВЛЕНО)
+    """
     Приложение по поиску адресов фишинговых сайтов.
     На вход требуется подать одно или несколько 
     # слов. Программа модифицирует их по 4 алгоритмам,
@@ -42,15 +44,29 @@ class DomainNameSearcher:
 
     Использование:
     >>>python3 dns_searcher.py word1 word2 ... wordN
-    '''
+    """
+
+    ZONES = [ 
+            'com', 'ru', 'net', 'org', 'info', 'cn', 'es', 
+            'top', 'au', 'pl', 'it', 'uk', 'tk', 'ml', 'ga',
+            'cf', 'us', 'xyz', 'top', 'site', 'win', 'bid'
+        ]
+
+    DATA_FORMAT = '%H.%M.%S-%d.%m.%Y'
+
+    def __init__(self):
+
+        self.report = dict()
 
     # TODO зачем self если он не используется? Сделать все эти функции статическими.
-    def _argument_parser(self):
-        '''
+    
+    @staticmethod
+    def _argument_parser():
+        """
         Функция, которая занимается обработкой входных
         параметров
         :return: Список слов
-        '''
+        """
 
         res = []
 
@@ -61,33 +77,37 @@ class DomainNameSearcher:
         return res
 
     def _get_extended_word_set(self, word_set):
-        '''
+        """
         Функция, модифицирующая входной набор слов
         по 4 различным стратегиям
         :param word_set: Поданный на вход набор слов
         :return: Список модифицированных слов
-        '''
+        """
+
         res = []
         res.extend(word_set)
 
         for word in word_set:
+
             # TODO нарушение PEP8 -- пробел после # должен быть!
-            # res.extend(self._letter_adder(word))
+
+            res.extend(self._letter_adder(word))
             # res.extend(self._symbol_changer(word))
             # res.extend(self._point_adder(word))
-            res.extend(self._symbol_remover(word))
+            # res.extend(self._symbol_remover(word))
             pass
 
         return res
 
-    def _letter_adder(self, word):
-        '''
+    @staticmethod
+    def _letter_adder(word):
+        """
         Стратегия изменения слова, добавлением в 
         конец одной дополнительной буквы
         :param word: str - Немодифицированное слово
         :return: Список модифицированных слов, 
         полученных из исходного
-        '''
+        """
 
         modified_word = []
 
@@ -99,14 +119,15 @@ class DomainNameSearcher:
 
         return modified_word
 
-    def _symbol_changer(self, word):
-        '''
+    @staticmethod
+    def _symbol_changer(word):
+        """
         Стратегия изменения слова, заменой одной
         буквы на другую, схожую по внешнему виду
         :param word: str - Немодифицированное слово
         :return: Список модифицированных слов, 
         полученных из исходного
-        '''
+        """
 
         modified_word = []
 
@@ -145,13 +166,14 @@ class DomainNameSearcher:
 
         return modified_word
 
-    def _point_adder(self, word):
-        '''
+    @staticmethod
+    def _point_adder(word):
+        """
         Стратегия изменения слова, выделением поддомена
         :param word: str - Немодифицированное слово
         :return: Список модифицированных слов, 
         полученных из исходного
-        '''
+        """
 
         modified_word = []
 
@@ -169,13 +191,14 @@ class DomainNameSearcher:
 
         return modified_word
 
-    def _symbol_remover(self, word):
-        '''
+    @staticmethod
+    def _symbol_remover(word):
+        """
         Стратегия изменения слова, удалением одной буквы
         :param word: str - Немодифицированное слово
         :return: Список модифицированных слов, 
         полученных из исходного
-        '''
+        """
 
         modified_word = []
         tmp = word
@@ -194,36 +217,36 @@ class DomainNameSearcher:
         return modified_word
 
     def _domain_zone_adder(self, extended_word_set):
-        '''
+        """
         Функция для составления доменов из списка
         измененных слов
         :param extended_word_set: Расширенный список
         слов (немодифицированных и модифицированных)
         :return: Список доменов
-        '''
+        """
 
         # TODO здесь неудобно их править! Вынести в глобальные переменные!
-        zones = [ 
-            'com', 'ru', 'net', 'org', 'info', 'cn', 'es', 
-            'top', 'au', 'pl', 'it', 'uk', 'tk', 'ml', 'ga',
-            'cf', 'us', 'xyz', 'top', 'site', 'win', 'bid'
-        ]
+        # zones = [ 
+        #     'com', 'ru', 'net', 'org', 'info', 'cn', 'es', 
+        #     'top', 'au', 'pl', 'it', 'uk', 'tk', 'ml', 'ga',
+        #     'cf', 'us', 'xyz', 'top', 'site', 'win', 'bid'
+        # ]
 
         urls = []
 
         for word in extended_word_set:
-            for zone in zones:
+            for zone in self.ZONES:
                 url = word + '.' + zone
                 urls.append(url)
 
         return urls
 
     def _multithread_dns_resolve(self, urls):
-        '''
+        """
         Функция для многопотокового выполнения
         dns-запросов
         :param urls: Список доменов
-        '''
+        """
 
         threads_num = 6
         threads = []
@@ -231,8 +254,9 @@ class DomainNameSearcher:
         for i in range(threads_num):
 
             t = threading.Thread(
-                target=self._thread_run, 
-                args=(i, urls, threads_num))
+                    target=self._thread_run, 
+                    args=(i, urls, threads_num)
+                )
 
             t.start()
 
@@ -242,12 +266,12 @@ class DomainNameSearcher:
             t.join()
 
     def _thread_run(self, thread_index, urls, threads_num):
-        '''
+        """
         Функция одного потока
         :param thread_index: Индекс потока
         :param urls: Список доменов
         :param threads_num: Количество потоков
-        '''
+        """
 
         while thread_index < len(urls):
             url = urls[thread_index]
@@ -261,12 +285,13 @@ class DomainNameSearcher:
 
             thread_index += threads_num
 
-    def _resolve_dns(self, url):
-        '''
+    @staticmethod
+    def _resolve_dns(url):
+        """
         Получение dns-записи по данному домену
         :param url: Домен
         :return dns_records: Лист адресов этого домена
-        '''
+        """
 
         dns_records = []
 
@@ -290,11 +315,14 @@ class DomainNameSearcher:
 
         return dns_records
 
-    def _print_result(self):
-        '''
-        Функция вывода в консоль результатов работы программы
-        '''
+    def _get_result(self):
+        """
+        Функция получения результатов работы программы
+        в красивом виде
+        :return: Таблица типа  PrettyTable
+        """
 
+        '''
         counter = 0
 
         # TODO:: from prettytable import PrettyTable. Аналогично в _report_to_file_human_readable(...)
@@ -304,67 +332,69 @@ class DomainNameSearcher:
         for key in self.report:
             counter += 1
             print('{:<5}{:<20}{}'.format(counter, key, str(self.report[key])))
+        '''
 
-        print('You can find all results in file report-current time and date')
+        x = PrettyTable(['n', 'domain', 'address'])
+
+        counter = 0
+
+        for key in self.report:
+            counter += 1
+
+            x.add_row([counter, key, str(self.report[key][0])])
+
+            for i in range(1, len(self.report[key])):
+
+                x.add_row(['', '', str(self.report[key][i])])
+
+        return x
 
     def _report_to_file(self):
-        '''
+        """
         Функция записи в файл результатов работы программы
         в удобном для последующего парсинга виде
-        '''
+        """
 
-        import datetime
+        b = datetime.datetime.now().strftime(self.DATA_FORMAT)
 
-        b = datetime.datetime.now().strftime('%H.%M.%S-%d.%m.%Y')
-
-        filename = 'report-' + b + '.txt'
+        filename = 'report-{}.txt'.format(b)
 
         with open(filename, 'w') as f:
             for key in self.report:
                 f.write('{}*{}\n'.format(key, str(self.report[key])))
 
     def _report_to_file_human_readable(self):
-        '''
+        """
         Функция записи в файл результатов работы программы
         в удобном для чтения человеком виде
-        '''
+        """
 
         # TODO что за @##%(%& !! Вынести в глобальные переменные!
-        import datetime
+        # import datetime
 
         # TODO второй раз вижу '%H.%M.%S-%d.%m.%Y' -- вынести в DATA_FORMAT = '%H.%M.%S-%d.%m.%Y' в глобальные переменные
-        b = datetime.datetime.now().strftime('%H.%M.%S-%d.%m.%Y')
+        
+        b = datetime.datetime.now().strftime(self.DATA_FORMAT)
 
         # TODO нужно так:
         # filename = 'report-{}_human_readable.txt'.format(b)
-        filename = 'report-' + b + '_human_readable.txt'
+
+        filename = 'report-{}_human_readable.txt'.format(b)
+
         # TODO и зачем вообще "human_readable"? В питоне все по умолчанию human readable !
 
         with open(filename, 'w') as f:
-            counter = 0
-
-            f.write('{:<5}{:<20}{}\n'.format('n', 'domain', 'address'))
-            f.write('====|===================|===================\n')
-
-            for key in self.report:
-                counter += 1
-                # TODO неудобно читать. Скобка ")" сверху на одном уровне со строкой, где "("  (ИСПРАВЛЕНО)
-                f.write(
-                    '{:<5}{:<20}{}\n'.format(
-                        counter, 
-                        key, 
-                        str(self.report[key])
-                    )
-                )
+            f.write(str(self._get_result()))
+            
 
     def run(self):
-        '''
+        """
         Основная функция программы, выполняющая запуск всех
         необходимых функций в нужном порядке
-        '''
+        """
 
         # TODO нет в __init__ self.report=None.
-        self.report = {}
+        # self.report = {}
 
         # Парсинг аргуменов и составление первичного списка слов
         word_set = self._argument_parser()
@@ -379,7 +409,7 @@ class DomainNameSearcher:
         self._multithread_dns_resolve(urls)
 
         # Вывод результата на экран и в файлы
-        self._print_result()
+        print(self._get_result())
         self._report_to_file()
         self._report_to_file_human_readable()
 
